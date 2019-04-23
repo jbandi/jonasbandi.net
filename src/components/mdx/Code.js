13f9c132b4698474fcf84f6104b2c39890ffa7ee
Jonas Bandi <dev@jonasbandi.net>
2019-04-23 21:52:52 +0200

    Code theme and line highliting

--- src/components/mdx/Code.js
+++ src/components/mdx/Code.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import { css } from '@emotion/core'
-import theme from 'prism-react-renderer/themes/oceanicNext'
+// import theme from 'prism-react-renderer/themes/oceanicNext'
+import theme from 'prism-react-renderer/themes/nightOwl'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
-const Code = ({ codeString, language, ...props }) => {
+const RE = /{([\d,-]+)}/
+
+function calculateLinesToHighlight(meta) {
+  if (RE.test(meta)) {
+    const lineNumbers = RE.exec(meta)[1]
+      .split(',')
+      .map(v => v.split('-').map(y => parseInt(y, 10)))
+    return index => {
+      const lineNumber = index + 1
+      const inRange = lineNumbers.some(([start, end]) =>
+        end ? lineNumber >= start && lineNumber <= end : lineNumber === start,
+      )
+      return inRange
+    }
+  } else {
+    return () => false
+  }
+}
+
+
+const Code = ({ codeString, language, metastring, ...props }) => {
   if (props['react-live']) {
     return (
       <LiveProvider code={codeString} noInline={true}>
@@ -14,6 +35,7 @@ const Code = ({ codeString, language, ...props }) => {
       </LiveProvider>
     )
   } else {
+    const shouldHighlightLine = calculateLinesToHighlight(metastring)
     return (
       <Highlight
         {...defaultProps}
@@ -24,7 +46,11 @@ const Code = ({ codeString, language, ...props }) => {
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
           <pre className={className} style={style}>
             {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
+              <div {...getLineProps({
+                line,
+                key: i,
+                className: shouldHighlightLine(i) ? 'highlight-line' : '',
+              })}>
                 <span
                   css={css`
                     display: inline-block;
