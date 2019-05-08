c64ffe8ff8ef0a569893fe2a4da823e291280193
Jonas Bandi <dev@jonasbandi.net>
2019-05-09 00:06:16 +0200

    trailing slashes in urls

--- src/pathFactory.js
+++ src/pathFactory.js
@@ -1,11 +1,12 @@
 const path = require('path')
 
 exports.generatePathForBlog = function(node) {
-  return `/blog`  + '/' + node.fields.date.split('T')[0] + '-' + node.fields.slug;
+  const date = node.fields.date.split('T')[0];
+  return `/blog/${date}-${node.fields.slug}/`;
 }
 
 exports.generatePathForDevlink = function(node){
   const extension = path.extname(node.fileAbsolutePath);
   const fileName = path.basename(node.fileAbsolutePath, extension);
-  return `/devlinks/${fileName}`;
+  return `/devlinks/${fileName}/`;
 }
