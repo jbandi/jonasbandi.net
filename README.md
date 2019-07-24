2b04909834ebe6a9647e40d944a34e6b9246d15b
Jonas Bandi <dev@jonasbandi.net>
2019-07-25 01:30:31 +0200

    Removing everything because of casing problems

--- README.md
+++
@@ -1,3 +0,0 @@
-# Jonas Bandi personal website
-
-The initial sources come from (https://github.com/kentcdodds/kentcdodds.com).
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import {Homepage} from '../components/homepage/Homepage'
-
-export default function i() {
-  return <Homepage />
-}
