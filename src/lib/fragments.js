3425180df9865e7ca27c4b2e7953f2ab573804a9
Jonas Bandi <dev@jonasbandi.net>
2019-07-25 00:59:25 +0200

    Pretty good shape

--- src/lib/fragments.js
+++ src/lib/fragments.js
@@ -3,14 +3,14 @@ import {graphql} from 'gatsby'
 export const bannerImage = graphql`
   fragment bannerImage640 on File {
     childImageSharp {
-      fluid(maxWidth: 640, traceSVG: {color: "#573ede"}) {
+      fluid(maxWidth: 640, traceSVG: {color: "#ff7622"}) {
         ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
   }
   fragment bannerImage720 on File {
     childImageSharp {
-      fluid(maxWidth: 720, traceSVG: {color: "#573ede"}, quality: 75) {
+      fluid(maxWidth: 720, traceSVG: {color: "#ff7622"}, quality: 75) {
         ...GatsbyImageSharpFluid_withWebp_tracedSVG
       }
     }
